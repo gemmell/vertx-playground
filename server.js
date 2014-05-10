a951f2527f4e03be562e24e8fd6a1695a0f3463e
Simon Gemmell <simon.gemmell@tkbt.com>
2014-05-10 13:32:36 +1000

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
   var vertx = require('vertx');
+  var container = require('vertx/container');
 
   vertx.createHttpServer().requestHandler(function(req) {
       var file = req.path() === '/' ? 'web/index.html' : req.path();
       req.response.sendFile('web/' + file);
-  }).listen(8080)
+  }).listen(parseInt(container.env.get('PORT')));
